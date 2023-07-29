1fcb23277224812651fd1f9a5c9c005c9134c62b
Андрей <parabellum.koval@gmail.com>
2023-07-29 15:28:10 +0300

    fix common temp

--- src/app/PageTemplates/Template.php
+++ src/app/PageTemplates/Template.php
@@ -3,29 +3,14 @@
 namespace App\PageTemplates;
 
 use App\PageTemplates\Traits\HomeTrait;
-use App\PageTemplates\Traits\AdvantagesTrait;
-use App\PageTemplates\Traits\AboutUsTrait;
-use App\PageTemplates\Traits\RewardsTrait;
-use App\PageTemplates\Traits\ContactsTrait;
-use App\PageTemplates\Traits\FaqTrait;
 
 class Template {
 
   use HomeTrait;
-  use AdvantagesTrait;
-  use AboutUsTrait;
-  use RewardsTrait;
-  use ContactsTrait;
-  use FaqTrait;
 
   public static $templates = [
     'common' => 'Общий',
-    'home' => 'Главная страница',
-    'advantages' => 'Преимущества',
-    'about_us' => 'О Нас',
-    'rewards' => 'Зарабатывайте с нами',
-    'contacts' => 'Контакты',
-    'faq' => 'Вопрос-ответ'
+    'home' => 'Главная страница'
   ];
 
   public static function useTemplate($name, $crud) {
@@ -39,6 +24,8 @@ class Template {
         'label' => trans('pages.content'),
         'type' => 'ckeditor',
         'placeholder' => trans('pages.content_placeholder'),
+        'fake' => true,
+        'store_in' => 'extras',
         'tab' => 'Основное',
       ]);
   }
